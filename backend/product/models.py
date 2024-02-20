4df99e1ef30bb9eed758636cb26cb2547fd126a0
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-02-19 21:17:13 -0300

    chore: Added model Produto + linting.

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.nome)
--- backend/product/tests/test_models.py
+++ backend/product/tests/test_models.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
-from myapp.models import Produto
+from product.models import Produto
 
 
 class TestProduto(TestCase):
     def setUp(self):
         self.produto_1 = Produto.objects.create(
-            nome="Avaianas Azul", descricao="Sandalia Avaianas azul", valor=15.50
+            nome="Avaianas Azul", 
+            descricao="Sandalia Avaianas azul", 
+            valor=15.50
         )
         self.produto_2 = Produto.objects.create(
             nome="Avaianas Vermelha",
@@ -13,10 +15,14 @@ class TestProduto(TestCase):
             valor=15.40,
         )
         self.produto_3 = Produto.objects.create(
-            nome="Avaianas Preta", descricao="Sandalia Avaianas preta", valor=15.30
+            nome="Avaianas Preta", 
+            descricao="Sandalia Avaianas preta", 
+            valor=15.30
         )
         self.produto_4 = Produto.objects.create(
-            nome="Avaianas Branca", descricao="Sandalia Avaianas branca", valor=15.20
+            nome="Avaianas Branca", 
+            descricao="Sandalia Avaianas branca", 
+            valor=15.20
         )
 
     def test_check_produto_exist(self):
@@ -28,21 +34,21 @@ class TestProduto(TestCase):
         """Test if product attributes are correct"""
         self.assertEqual(type(self.produto_1.nome), str)
         self.assertEqual(type(self.produto_1.descricao), str)
-        self.assertEqual(type(self.produto_1.valor), bool)
+        self.assertEqual(type(self.produto_1.valor), float)
 
     def test_values_created_into_produto_table(self):
         """Test if values from instance created are inserted into db"""
         product_quantity = Produto.objects.all().count()
         self.assertEqual(product_quantity, 4)
         self.assertEqual(self.produto_1.valor, 15.50)
-        self.assertEqual(self.produto_1.name, "Avaianas Azul")
+        self.assertEqual(self.produto_1.nome, "Avaianas Azul")
         self.assertEqual(self.produto_1.descricao, "Sandalia Avaianas azul")
         self.assertEqual(self.produto_2.valor, 15.40)
-        self.assertEqual(self.produto_2.name, "Avaianas Vermelha")
+        self.assertEqual(self.produto_2.nome, "Avaianas Vermelha")
         self.assertEqual(self.produto_2.descricao, "Sandalia Avaianas vermelha")
-        self.assertEqual(self.produto_2.valor, 15.30)
-        self.assertEqual(self.produto_2.name, "Avaianas Branca")
-        self.assertEqual(self.produto_2.descricao, "Sandalia Avaianas preta")
-        self.assertEqual(self.produto_2.valor, 15.20)
-        self.assertEqual(self.produto_2.name, "Avaianas Branca")
-        self.assertEqual(self.produto_2.descricao, "Sandalia Avaianas branca")
+        self.assertEqual(self.produto_3.valor, 15.30)
+        self.assertEqual(self.produto_3.nome, "Avaianas Preta")
+        self.assertEqual(self.produto_3.descricao, "Sandalia Avaianas preta")
+        self.assertEqual(self.produto_4.valor, 15.20)
+        self.assertEqual(self.produto_4.nome, "Avaianas Branca")
+        self.assertEqual(self.produto_4.descricao, "Sandalia Avaianas branca")
