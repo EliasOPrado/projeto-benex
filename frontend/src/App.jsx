764cff3da79254556d3a42bddc4165a4523ef27b
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-02-21 17:02:55 -0300

    chore: Added update functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [value, setValue] = useState(0.0);
+  const [description, setDescription] = useState("");
+  const [showForm, setShowForm] = useState(false);
+  const [currentId, setCurrentItemId] = useState(null)
+
 
   const fetchData = async () => {
     try {
@@ -37,13 +43,125 @@ function App() {
     }
   };
 
+  // make the update request
+  const handleUpdate = async (event) => {
+    event.preventDefault();
 
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/produtos/${currentId}/`, {
+        method: 'PUT', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nome: name,
+          valor: value,
+          descricao: description,
+        }),
+      });
+
+      if (response.ok) {
+        fetchData();
+        setShowForm(false);
+        setCurrentItemId(null);
+      } else {
+        console.error('Error updating item:', response.status);
+      }
+    } catch (error) {
+      console.error('Error updating item:', error);
+    }
+  };
+
+    // make the post request
+    const handlePost = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/api/produtos/`, {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            nome: name,
+            valor: value,
+            descricao: description,
+          }),
+        });
+  
+        if (response.ok) {
+          fetchData();
+          setShowForm(false);
+          setCurrentItemId(null);
+        } else {
+          console.error('Error updating item:', response.status);
+        }
+      } catch (error) {
+        console.error('Error updating item:', error);
+      }
+    };
+  
+  // functions for form inputs that will
+  // be used to send to the request..
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const handleValueChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  // send the current item from the list
+  // to the form
+  const handleEditChange = (instance) => {
+    setName(instance.nome);
+    setValue(instance.valor);
+    setCurrentItemId(instance.id);
+    setDescription(instance.descricao);
+    setShowForm(true);
+  }
 
   return (
     <div className="main-container">
       <div className="navbar">
         <h3 className="logo">Bnex Challenge</h3>
+        {showForm &&
+        <button type="submit" className="update-button-navbar" onClick={() => setShowForm(false)}>Back to products</button>
+        }
       </div>
+      {showForm ?
+        <div className="form-container">
+          <h3>Update Product</h3>
+          <form onSubmit={handleUpdate} className="form">
+            <input 
+            type="text" 
+            name="nome"
+            value={name}
+            onChange={handleNameChange}
+            className="main-input" />
+            <input 
+            type="number" 
+            name="valor" 
+            value={value}
+            onChange={handleValueChange}
+            className="main-input" />
+            <textarea 
+            rows="5" 
+            cols="33"
+            onChange={handleDescriptionChange}
+            value={description}
+            className="text-area-input"></textarea>
+            <button 
+            type="submit" 
+            className="update-button"
+            >Update</button>
+          </form>
+        </div>
+        :
       <div className="cards-container">
         {products.map((product) => (
           <div className="card" key={product.id}>
@@ -54,12 +172,13 @@ function App() {
               <p className="card-description">{product.descricao}</p>
             </div>
             <div className="card-right-side">
-              <i className="fa-regular fa-pen-to-square product-icon"></i>
+              <a className="product-icon" href="#" onClick={() => handleEditChange(product)}><i className="fa-regular fa-pen-to-square product-icon"></i></a>
               <a className="product-icon" href="#" onClick={() => handleDelete(product.id)}><i className="fa-solid fa-delete-left"></i></a>
             </div>
           </div>
         ))}
       </div>
+      }
     </div>
   );
 }
